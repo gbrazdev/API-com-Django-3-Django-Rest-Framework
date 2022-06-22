2d78d096328e3f290c5f721be6626b3b268eea82
GABRIEL  BRAZ <gabrielbrazdev@gmail.com>
2022-06-22 15:38:10 -0300

    aula-03

--- escola/views.py
+++ escola/views.py
@@ -1,14 +1,13 @@
 from rest_framework import viewsets
 from escola.models import Aluno, Curso
-from serializer import AlunoSerializer, CursoSerializer
-
+from escola.serializer import AlunoSerializer, CursoSerializer
 
 class AlunosViewSet(viewsets.ModelViewSet):
-    """Exisbindo todos os alunos"""
-    queryset = Alunos.objects.all()
+    """Exibindo todos os alunos e alunas"""
+    queryset = Aluno.objects.all()
     serializer_class = AlunoSerializer
 
 class CursosViewSet(viewsets.ModelViewSet):
-    """Exisbindo todos os cusros"""
-    queryset = Cursos.objects.all()
+    """Exibindo todos os cursos"""
+    queryset = Curso.objects.all()
     serializer_class = CursoSerializer
--- setup/urls.py
+++ setup/urls.py
@@ -1,33 +1,13 @@
-"""setup URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, include
-# from thycotic_api.views import getData
-from escola.views import AlunosViewSet, CursoViewset
-from rest_framework import routes
+from django.urls import path,include
+from escola.views import AlunosViewSet, CursosViewSet
+from rest_framework import routers
 
-
-router = router.DefaultRouter()
+router = routers.DefaultRouter()
 router.register('alunos', AlunosViewSet, basename='Alunos')
-router.register('cursos', AlunosViewSet, basename='Cursos')
-
+router.register('cursos', CursosViewSet, basename='Cursos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    # path('alunos/', alunos),
-    # path('token', getData),
+    path('', include(router.urls) )
 ]
